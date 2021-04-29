56be8b2037d22e7e0f3bf74da1dc0151787f2eec
DomCastaneda <dcastaneda2828@gmail.com>
2021-04-28 22:37:36 -0700

    Added Some Walls For Testing

--- Assets/Scripts/EnemyProjectile.cs
+++ Assets/Scripts/EnemyProjectile.cs
@@ -29,6 +29,10 @@ public class EnemyProjectile : MonoBehaviour
         {
             return;
         }
+        else if (other.gameObject.tag == "Wall")
+        {
+            Destroy(this.gameObject);
+        }
         else if (other.gameObject.tag == "Player")
         {
             other.GetComponent<Player>().TakeDamage(damage);
