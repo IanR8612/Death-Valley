a762a10c0a436afc3031799c7d734a6d199c687a
DomCastaneda <dcastaneda2828@gmail.com>
2021-05-06 17:21:22 -0700

    Enemy Healthbar Fixed

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,20 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
-    private int health = 5;
-    public void TakeDamage(int damage)
+    public float Health;
+    public float MaxHealth = 5;
+
+    public HealthbarBehavior Healthbar;
+
+    void Start()
     {
-        health -= damage;
-        if (health <= 0)
+        Health = MaxHealth;
+    }
+
+    void Update()
+    {
+        Healthbar.SetHealth(Health, MaxHealth);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        Health -= damage;
+        Healthbar.SetHealth(Health, MaxHealth);
+        if (Health <= 0)
             Die();
     }
 
     private void Die()
     {
-        Destroy(this.gameObject);        
+        Destroy(this.gameObject);
         GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().AddToKillCount();
     }
 }
