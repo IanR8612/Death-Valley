8b027b6ada5fe094dab4365bd35d31116eed2e73
Jaewon Park <jaepark@chapman.edu>
2021-05-09 18:39:02 -0700

    PlayerHealth
    Still trying to fix some errors

---
+++ Assets/Scripts/FillStatusBar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillStatusBar : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public Image fillImage;
+    private Slider slider;
+
+
+    void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(slider.value <= slider.minValue)
+        {
+            fillImage.enabled = false;
+        }
+        
+        if (slider.value > slider.minValue && !(fillImage.enabled))
+        {
+            fillImage.enabled = true;
+        }
+
+        float fillValue = playerHealth.currentHealth / playerHealth.maxHealth;
+
+        if(fillValue <= slider.maxValue / 3)
+        {
+            fillImage.color = Color.white;
+        }
+        else if (fillValue > slider.maxValue / 3)
+        {
+            fillImage.color = Color.red;
+        }
+
+
+        slider.value = fillValue; 
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 3;
+    public int currentHealth;
+
+    // public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+
+        //if(currentHealth <= 0)
+        //{
+        //    // Dead
+
+        //}
+    }
+
+    void Heal(int amount)
+    {
+        currentHealth += amount;
+
+        if(currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+
+        }
+    }
+
+}
