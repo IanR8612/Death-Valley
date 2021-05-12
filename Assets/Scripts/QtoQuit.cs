b4f469dcaf560229144d2e1daff70ea68b4dbc49
Nathaniel Cuadros <54486315+RatFuryJunior@users.noreply.github.com>
2021-05-11 23:23:14 -0700

    Build Day Push :)
    added q to quit imp and script, added r to reset script, changed a bit of stuff in main with level design, gun spawn is in center now

---
+++ Assets/Scripts/QtoQuit.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QtoQuit : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/RtoReload.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RtoReload : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+            //run reset commands for other scripts
+        }
+    }
+}
