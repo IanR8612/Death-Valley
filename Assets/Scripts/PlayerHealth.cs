7f2284a7a30aac0526028dfc107dbec90a1c45f9
DomCastaneda <dcastaneda2828@gmail.com>
2021-05-18 17:35:33 -0700

    Fixed Bug Where Player Wasn't Dying

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -20,7 +20,7 @@ public class PlayerHealth : MonoBehaviour
     {
         currentHealth -= amount;
 
-        if (currentHealth == 0)
+        if (currentHealth <= 0)
         {
             playerToDie.Die();
         }
