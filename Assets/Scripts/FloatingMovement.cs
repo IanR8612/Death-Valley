3b5fd10d3156b5a05916e1be4a67fac419955d04
Nathaniel Cuadros <54486315+RatFuryJunior@users.noreply.github.com>
2021-05-19 20:53:05 -0700

    Floating Movment for healthkit

---
+++ Assets/Scripts/FloatingMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingMovement : MonoBehaviour
+{
+    private float floatingTimer = 0f;
+    public float floatingMax = 1.25f;
+    public float floatingDirection = 0.1f;
+
+    private void FixedUpdate()
+    {
+        if (floatingTimer < floatingMax)
+        {
+            transform.position = new Vector2(transform.position.x, 
+                transform.position.y + (Time.fixedDeltaTime * floatingDirection));
+            floatingTimer += Time.fixedDeltaTime;
+        }
+        else
+        {
+            floatingDirection *= -1;
+            floatingTimer = 0f;
+        }
+    }
+}
